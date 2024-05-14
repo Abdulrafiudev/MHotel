04d6a8be34940510a1d03c3f8b924b40338df569
Abdulrafiu-Python <abolajiibrahim70@gmail.com>
2024-05-14 13:34:48 +0100

    completed catalogue

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react"
 import "./App.css"
 import Header from "./components/header/header"
 import Intro from "./components/intro/intro"
+import Catalogue from "./components/catalogue/catalogue"
 
 function App() {
   return (
     <>
       <Header />
       <Intro />
+      <Catalogue />
     </>
   )
 }
---
+++ src/components/catalogue/catalogue.jsx
@@ -0,0 +1,57 @@
+import React from "react"
+import "./catalogue.css"
+import vector_1 from "../../assets/images/vector_1.png"
+import vector_2 from "../../assets/images/vector_2.png"
+import vector_3 from "../../assets/images/vector_3.png"
+import vector_4 from "../../assets/images/vector_4.png"
+
+function Catalogue() {
+  let catalogue_cart = [
+    {
+      image: vector_1,
+      text_1: "Get Comfort",
+      text_2: "Get Comfort",
+    },
+    {
+      image: vector_2,
+      text_1: "Dinning",
+      text_2: "Get Comfort",
+    },
+    {
+      image: vector_3,
+      text_1: "Event Facilities",
+      text_2: "Get Comfort",
+    },
+    {
+      image: vector_4,
+      text_1: "Recreational",
+      text_2: "Get Comfort",
+    },
+  ]
+  return (
+    <>
+      <div className="catalogue_container">
+        <div className="catalogue">
+          <span> Marina Haven</span>
+          <div className="catalogue_box_container">
+            <div className="catalogue_align_box">
+              {catalogue_cart.map((item, index) => {
+                return (
+                  <div className="catalogue_box" key={index}>
+                    <img src={item.image} />
+                    <div className="text_box">
+                      <span> {item.text_1} </span>
+                      <span> {item.text_2} </span>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Catalogue
