344432e98cd1563002e49bd6eaa8b48dee35acbe
Abdulrafiu-Python <abolajiibrahim70@gmail.com>
2024-05-18 08:51:48 +0100

    checking

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,24 +1,36 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import "./intro.css"
 import hotel_image from "../../assets/images/hotel_image.jpg"
-import { motion } from "framer-motion"
+import gsap from "gsap"
+import { Power3, Power2, Power0, Power1 } from "gsap"
+import { useGSAP } from "@gsap/react"
+
+let tl = gsap.timeline()
 
 function Intro() {
+  let container = useRef(null)
+  useGSAP(() => {
+    let image_container = container.current.firstElementChild
+    let img = container.current.firstElementChild.firstElementChild
+    console.log(image_container)
+    tl.from(image_container, {
+      yPercent: -100,
+      duration: 1,
+
+      ease: Power1.easeIn,
+    }).from(img, { scale: 1.6, duration: 1.5, easeIn: Power0.easeIn }, 0.9)
+  })
+
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0, y: "-100vh" }}
-        whileInView={{ y: "0", opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="intro_container"
-      >
+      <div className="intro_container" ref={container}>
         <div className="image_container">
           <img src={hotel_image} className="hotel_image" />
         </div>
         <div className="green_line_container">
           <div className="green_line_piece"></div>
         </div>
-      </motion.div>
+      </div>
     </>
   )
 }
